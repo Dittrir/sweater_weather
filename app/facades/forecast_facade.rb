ef204d29e74685d588327ffba139b1254607f00f
Robin Dittrich <robinodittrich@gmail.com>
2022-03-05 15:16:18 -0800

    Change long to lng.

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,7 +1,7 @@
 class ForecastFacade
   def self.get_forcast_for(location)
     coords = MapQuestService.get_lat_long_for(location)
-    forecast = ForecastService.get_forcast_for(coords[:lat], coords[:long])
+    forecast = ForecastService.get_forecast_for(coords[:lat], coords[:lng])
 
     Forecast.new(forecast)
   end
