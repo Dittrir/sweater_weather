389faca53cc41dc1396164e633fca0f973457ebe
Robin Dittrich <robinodittrich@gmail.com>
2022-03-05 13:49:07 -0800

    Create map quest service.

---
+++ app/services/map_quest_service.rb
@@ -0,0 +1,12 @@
+class MapQuestService
+  def self.conn
+    Faraday.new(url: "http://www.mapquestapi.com") do |faraday|
+      faraday.params["key"] = ENV['map_quest_key']
+    end
+  end
+
+  def self.get_lat_long_for(location)
+    response = conn.get("/geocoding/v1/address?location=#{location}")
+    JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
+  end
+end
