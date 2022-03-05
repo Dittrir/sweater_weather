52e7f44679130776e847c1f4c5c7d005260b10a7
Robin Dittrich <robinodittrich@gmail.com>
2022-03-05 15:14:48 -0800

    Minor refactor to map quest service.

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -7,6 +7,7 @@ class MapQuestService
 
   def self.get_lat_long_for(location)
     response = conn.get("/geocoding/v1/address?location=#{location}")
+
     JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
   end
 end
