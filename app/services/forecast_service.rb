4a7d6e1dec9b911e540365b92fe1861d74c73fbe
Robin Dittrich <robinodittrich@gmail.com>
2022-03-05 15:15:22 -0800

    Rename to ForecastService and add get_forecast_for method

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,12 +1,12 @@
-class MapQuestService
+class ForecastService
   def self.conn
     Faraday.new(url: "https://api.openweathermap.org") do |faraday|
       faraday.params["appid"] = ENV['open_weather_key']
     end
   end
 
-  def get_forcast_for(latitude, longitude)
-    response = conn.get("/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&exclude=minutely, alerts")
+  def self.get_forecast_for(latitude, longitude)
+    response = conn.get("/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&exclude=minutely, alerts&units=imperial")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
