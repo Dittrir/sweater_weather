eee80a50b860be6010f28ca46ef2cab87aa28f9f
Robin Dittrich <robinodittrich@gmail.com>
2022-03-06 14:34:48 -0800

    Add a sessions controller.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,10 +5,9 @@ class Api::V1::SessionsController < ApplicationController
     if user == nil
       render(json: {error: "User does not exist."}, status: 401 )
     elsif user.authenticate(params[:password])
-      render(json: SessionSerializer.new(Session.find(user.id)), status: :created)
+      render(json: SessionSerializer.new(User.find(user.id)), status: :created)
     else
       render(json: {error: "Email or password is incorrect. Please try again."}, status: 401 )
-      redirect_to "/login"
     end
   end
 end
