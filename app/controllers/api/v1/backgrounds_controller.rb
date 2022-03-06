c68b1c6dd3ff7bdb51dd5335322596f45c95a467
Robin Dittrich <robinodittrich@gmail.com>
2022-03-05 16:55:27 -0800

    Add background controller.

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    BackgroundFacade.get_background_for(params[:location])
-    render(json: BackgroundSerializer.new(forecast))
+    background = BackgroundFacade.get_background_for(params[:location])
+    render(json: BackgroundSerializer.new(background))
   end
 end
