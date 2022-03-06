83745bac9b1431f07944b4e24e526a4a0de8ee12
Robin Dittrich <robinodittrich@gmail.com>
2022-03-05 18:14:06 -0800

    Add background facade.

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -1,6 +1,6 @@
 class BackgroundFacade
   def self.get_background_for(location)
-    background = BackgroundService.get_background_for(coords[:lat], coords[:lng])
+    background = BackgroundService.get_background_for(location)
 
     Background.new(background, location)
   end
