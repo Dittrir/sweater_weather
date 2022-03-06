400ec800d37c78fd1a5e0c71e90a6f58b1ce615a
Robin Dittrich <robinodittrich@gmail.com>
2022-03-05 16:49:47 -0800

    Add tests for forecast request.

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'The weather forcast API' do
   describe "returns a forecast for a specific location" do
-    it 'happy path' do
+    it 'happy path: current_weather' do
       location = "Bellingham, WA"
 
       get "/api/v1/forecast?location=#{location}"
@@ -12,18 +12,108 @@ RSpec.describe 'The weather forcast API' do
       forecast = JSON.parse(response.body, symbolize_names: true)
       forecast_data = forecast[:data]
 
-      forecast_data.each do |forecast|
-        expect(forecast).to have_key(:id)
-        expect(forecast[:id].to_i).to be_an(Integer)
+      expect(forecast_data).to have_key(:id)
+      expect(forecast_data[:id]).to eq(nil)
 
-        expect(forecast[:attributes]).to have_key(:name)
-        expect(forecast[:attributes][:name]).to be_a(String)
+      expect(forecast_data[:attributes]).to have_key(:current_weather)
+      expect(forecast_data[:attributes][:current_weather]).to be_a(Hash)
 
-        expect(forecast[:attributes]).to have_key(:description)
-        expect(forecast[:attributes][:description]).to be_a(String)
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:datetime)
+      expect(forecast_data[:attributes][:current_weather][:datetime]).to be_a(String)
 
-        expect(forecast[:attributes]).to have_key(:unit_price)
-        expect(forecast[:attributes][:unit_price]).to be_a(Float)
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:sunrise)
+      expect(forecast_data[:attributes][:current_weather][:sunrise]).to be_a(String)
+
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:temperature)
+      expect(forecast_data[:attributes][:current_weather][:temperature]).to be_a(Float)
+
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:feels_like)
+      expect(forecast_data[:attributes][:current_weather][:feels_like]).to be_a(Float)
+
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:humidity)
+      expect(forecast_data[:attributes][:current_weather][:humidity]).to be_a(Integer)
+
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:uvi)
+      expect(forecast_data[:attributes][:current_weather][:uvi]).to be_a(Float)
+
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:visibility)
+      expect(forecast_data[:attributes][:current_weather][:visibility]).to be_a(Integer)
+
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:conditions)
+      expect(forecast_data[:attributes][:current_weather][:conditions]).to be_a(String)
+
+      expect(forecast_data[:attributes][:current_weather]).to have_key(:icon)
+      expect(forecast_data[:attributes][:current_weather][:icon]).to be_a(String)
+    end
+
+    it 'happy path: daily_weather' do
+      location = "Bellingham, WA"
+
+      get "/api/v1/forecast?location=#{location}"
+
+      expect(response).to be_successful
+
+      forecast = JSON.parse(response.body, symbolize_names: true)
+      forecast_data = forecast[:data]
+
+      expect(forecast_data).to have_key(:id)
+      expect(forecast_data[:id]).to eq(nil)
+
+      expect(forecast_data[:attributes]).to have_key(:daily_weather)
+      expect(forecast_data[:attributes][:daily_weather]).to be_a(Array)
+
+      forecast_data[:attributes][:daily_weather].each do |day|
+        expect(day).to have_key(:date)
+        expect(day[:date]).to be_a(String)
+
+        expect(day).to have_key(:sunrise)
+        expect(day[:sunrise]).to be_a(String)
+
+        expect(day).to have_key(:sunset)
+        expect(day[:sunset]).to be_a(String)
+
+        expect(day).to have_key(:max_temp)
+        expect(day[:max_temp]).to be_a(Float)
+
+        expect(day).to have_key(:min_temp)
+        expect(day[:min_temp]).to be_a(Float)
+
+        expect(day).to have_key(:conditions)
+        expect(day[:conditions]).to be_a(String)
+
+        expect(day).to have_key(:icon)
+        expect(day[:icon]).to be_a(String)
+      end
+    end
+
+    it 'happy path: hourly_weather' do
+      location = "Bellingham, WA"
+
+      get "/api/v1/forecast?location=#{location}"
+
+      expect(response).to be_successful
+
+      forecast = JSON.parse(response.body, symbolize_names: true)
+      forecast_data = forecast[:data]
+
+      expect(forecast_data).to have_key(:id)
+      expect(forecast_data[:id]).to eq(nil)
+
+      expect(forecast_data[:attributes]).to have_key(:hourly_weather)
+      expect(forecast_data[:attributes][:hourly_weather]).to be_a(Array)
+
+      forecast_data[:attributes][:hourly_weather].each do |day|
+        expect(day).to have_key(:time)
+        expect(day[:time]).to be_a(String)
+
+        expect(day).to have_key(:temperature)
+        expect(day[:temperature]).to be_a(Float)
+
+        expect(day).to have_key(:conditions)
+        expect(day[:conditions]).to be_a(String)
+
+        expect(day).to have_key(:icon)
+        expect(day[:icon]).to be_a(String)
       end
     end
   end
