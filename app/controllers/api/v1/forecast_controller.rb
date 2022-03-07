f076d823155279a0faa5eb5a531bcf1812205d3b
Robin Dittrich <robinodittrich@gmail.com>
2022-03-07 13:53:45 -0800

    Remove nil conditional from forecast facade.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    forecast = ForecastFacade.get_forcast_for(params[:location])
-    render(json: ForecastSerializer.new(forecast))
+    if params[:location].present?
+      forecast = ForecastFacade.get_forcast_for(params[:location])
+
+      render(json: ForecastSerializer.new(forecast))
+    else
+      render(json: {data: {message: "Insufficent query parameters"}}, status: 400 )
+    end
   end
 end
