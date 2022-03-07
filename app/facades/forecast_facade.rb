3d20eb965035cb08c44fc310f934caa74a6c187e
Robin Dittrich <robinodittrich@gmail.com>
2022-03-06 19:23:04 -0800

    Refactor forecast facade.

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -2,7 +2,7 @@ class ForecastFacade
   def self.get_forcast_for(location)
     coords = MapQuestService.get_lat_long_for(location)
     forecast = ForecastService.get_forecast_for(coords[:lat], coords[:lng])
-
+  
     Forecast.new(forecast)
   end
 end
