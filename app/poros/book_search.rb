43d7a3cb9174ac46fd768433bba02dc13715b33e
Robin Dittrich <robinodittrich@gmail.com>
2022-03-07 09:42:05 -0800

    Add book search poro to format data into json.

---
+++ app/poros/book_search.rb
@@ -0,0 +1,10 @@
+class BookSearch
+  attr_reader :id, :destination, :forecast, :total_books_found, :books
+  def initialize(data)
+    @id = nil
+    @destination = data[:destination]
+    @forecast = data[:forecast]
+    @total_books_found = data[:total_books_found]
+    @books = data[:books]
+  end
+end
