8a3b3c5b13a4e72518938dc4a7b7939baba2448f
Robin Dittrich <robinodittrich@gmail.com>
2022-03-06 19:22:45 -0800

    Create a road trip serializer.

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,9 @@
+class RoadTripSerializer
+  include JSONAPI::Serializer
+  set_type :id
+  set_type "roadtrip"
+  attributes :start_city,
+             :end_city,
+             :travel_time,
+             :weather_at_eta
+end
