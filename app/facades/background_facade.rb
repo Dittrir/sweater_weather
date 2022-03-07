f3107f85aeb6d54d58db0c4aafbea8bb9e0bc1f8
Robin Dittrich <robinodittrich@gmail.com>
2022-03-07 13:39:07 -0800

    Add conditional to background facade to filter out nil results.

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -1,7 +1,8 @@
 class BackgroundFacade
   def self.get_background_for(location)
     background = BackgroundService.get_background_for(location)
-
-    Background.new(background, location)
+    if background[:total] > 0
+      Background.new(background, location)
+    end
   end
 end
