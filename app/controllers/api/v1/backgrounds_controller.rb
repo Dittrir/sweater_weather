d47f56691c47257aa2b5ce398b5a7f8376fc467c
Robin Dittrich <robinodittrich@gmail.com>
2022-03-07 13:39:30 -0800

    Add conditional to background controller for sad path testing.

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    background = BackgroundFacade.get_background_for(params[:location])
-    render(json: BackgroundSerializer.new(background))
+    if params[:location].present?
+      background = BackgroundFacade.get_background_for(params[:location])
+      if background.nil?
+        render(json: {data: {message: "There were no matches"}}, status: 200 )
+      else
+      render(json: BackgroundSerializer.new(background))
+    end
+    else
+      render(json: {data: {message: "Insufficent query parameters"}}, status: 400 )
+    end
   end
 end
